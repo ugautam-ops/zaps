89bd9d3f02aab4a3f988fc3c5c6abf719bb28028
Captain <captain@Captains-MacBook-Pro.local>
2025-12-10 16:12:22 +0545

    Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import CookieConsent from '@/components/CookieConsent';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Zap Mind - Test Your Cognitive Abilities",
